0ca16b2c144c7d1ee0ab864b65ffe0c1044e7d69
Archit-1344 <archit7307@gmail.com>
2026-03-10 09:43:36 +0530

    10/03/26

---
+++ ao.js
@@ -0,0 +1,10 @@
+let collection = [
+    undefined ,
+    null,
+    new Date(),
+    new String("hello"),
+]
+console.log(collection);
+collection.teachername = "Archit";
+collection.phonenumber = 9773601171 ;
+console.log(collection);
