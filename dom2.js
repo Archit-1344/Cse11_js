c41319db4fdc028e9bfe2b5f6c0dcdc0b51e15d4
Archit-1344 <archit7307@gmail.com>
2026-03-17 10:11:09 +0530

    17/03/26

---
+++ dom2.js
@@ -0,0 +1,18 @@
+function oc(){
+    console.log("clicked");
+}
+function omo(){
+       console.log("over");
+}
+function omou(){
+       console.log("out");
+}
+function omd(){
+       console.log("down");
+}
+function omu(){
+       console.log("up");
+}
+function omm(){
+       console.log("move");
+}
