97e4bc6785fc86dea0d6bee204037f90de5dd911
Archit-1344 <archit7307@gmail.com>
2026-03-17 10:30:33 +0530

    17 march 26

--- dom2.js
+++ dom2.js
@@ -16,3 +16,19 @@ function omu(){
 function omm(){
        console.log("move");
 }
+var text = '{ "employees": [' +
+    '{ "firstName":"John","lastName":"Doe" },' +
+    '{ "firstName":"Jane","lastName":"Smith" },' +
+    '{ "firstName":"Alice","lastName":"Johnson" }' +
+'] }';
+
+var obj = JSON.parse(text);
+
+document.getElementById("demo").innerHTML =
+    obj.employees[0].firstName + " " + obj.employees[0].lastName;
+
+document.getElementById("demo").innerHTML += "<br>" +
+    obj.employees[1].firstName + " " + obj.employees[1].lastName;
+
+document.getElementById("demo").innerHTML += "<br>" +
+    obj.employees[2].firstName + " " + obj.employees[2].lastName;
